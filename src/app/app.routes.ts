31aeba7aa804475e1165d270149960f98e82c0f6
Tony HUerto <tonylex12@gmail.com>
2024-11-15 14:47:40 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { EjemploAngularMaterialComponent } from './ejemplo-angular-material/ejemplo-angular-material.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'angular-material',
+    component: EjemploAngularMaterialComponent,
+  },
+];
